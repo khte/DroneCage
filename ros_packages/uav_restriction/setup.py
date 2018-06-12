d1cf74281c4e2e9c21849df2e652099581bc7037
TobiasLundby <tobiaslundby@gmail.com>
2018-06-12 17:23:25 +0200

    Changes from scr to script

--- ros_packages/uav_restriction/setup.py
+++ ros_packages/uav_restriction/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-	scripts = ['src/uav_restriction_node.py'],
+	scripts = ['script/uav_restriction_node.py'],
 )
 
 setup(**d)
