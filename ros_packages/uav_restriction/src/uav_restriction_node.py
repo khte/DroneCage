c4e809702d318d62c5c6d552ec557f13d27faba3
TobiasLundby <tobiaslundby@gmail.com>
2018-06-13 21:14:55 +0200

    Added launch files and support for launch files

--- ros_packages/uav_restriction/src/uav_restriction_node.py
+++ ros_packages/uav_restriction/src/uav_restriction_node.py
@@ -11,7 +11,9 @@ from math import sin, cos, pi, sqrt
 import time
 import serial
 from multiprocessing import Lock
+import struct
 from utm import utmconv
+import sys
 from mavlink_lora.msg import mavlink_lora_pos, mavlink_lora_attitude
 
 # Defines
@@ -39,8 +41,8 @@ DRONECAGE_LIMITS = {
 }
 FACTOR_INPUT2METRIC_XY  = 0.01
 FACTOR_INPUT2METRIC_Z   = 0.1
-ARDUINO_BAUD_RATE       = 115200
-ARDUINO_PORT            = '/dev/ttyUSB2'
+ARDUINO_BAUD_RATE       = 115200 # NO LONGER USED
+ARDUINO_PORT            = '/dev/ttyUSB0' # NO LONGER USED
 ROS_POSE_TOPIC          = '/mavlink_pos'
 ROS_ATTITUDE_TOPIC      = '/mavlink_attitude'
 ROS_STATUS_TOPIC        = '/mavlink_status'
@@ -54,11 +56,17 @@ DIFF_DRONECAGE_RAD      = DIFF_DRONECAGE_DEG*DEG2RAD
 class uav_restriction():
     ROS_NODE_NAME = 'uav_restriction'
     mutex_heading = Lock()
-    def __init__(self, debug = False):
-        self.debug = debug
-
-        rospy.init_node(self.ROS_NODE_NAME)
+    def __init__(self, serial_device, serial_baudrate, debug = False):
+        rospy.init_node(self.ROS_NODE_NAME, log_level=rospy.DEBUG)
         rospy.loginfo('Started: '+rospy.get_caller_id())
+        #serial_device = str(serial_device)
+        #serial_baudrate = int(float(serial_baudrate))
+        serial_device = rospy.get_param('/uav_restriction_node/serial_device')
+        serial_baudrate = rospy.get_param('/uav_restriction_node/serial_baudrate')
+        rospy.logdebug('Device %s, data type %s' % (serial_device, type(serial_device)))
+        rospy.logdebug('Baud rate %s, data type %s' % (serial_baudrate, type(serial_baudrate)))
+
+        self.debug = debug
 
         rospy.loginfo('Configuring UTM for zone 32')
         self.uc = utmconv()
@@ -69,8 +77,8 @@ class uav_restriction():
 
         rospy.loginfo('Configuring arduino serial port')
         self.ser_arduino = serial.Serial()
-        self.ser_arduino.baudrate = ARDUINO_BAUD_RATE
-        self.ser_arduino.port = ARDUINO_PORT
+        self.ser_arduino.baudrate = serial_baudrate #ARDUINO_BAUD_RATE
+        self.ser_arduino.port = serial_device #ARDUINO_PORT
         #rospy.loginfo(ser)
         rospy.loginfo('Opening arduino serial port')
         try:
@@ -81,6 +89,13 @@ class uav_restriction():
             with self.mutex_heading:
                 self.uav_heading_rad = 0
                 self.uav_heading_deg = self.uav_heading_rad*RAD2DEG
+
+            if rospy.get_param('/uav_restriction_node/emulate_arduino_send'):
+                r = rospy.Rate(0.5) # 10hz
+                while not rospy.is_shutdown():
+                    rospy.loginfo('Sending')
+                    self.send_arduino_message(10,20,30,40,50,60)
+                    r.sleep()
 
             rospy.Subscriber(ROS_POSE_TOPIC, mavlink_lora_pos, self.callback_pos)
             rospy.Subscriber(ROS_ATTITUDE_TOPIC, mavlink_lora_attitude, self.callback_attitude)
@@ -98,7 +113,8 @@ class uav_restriction():
             #values = bytearray([b'P', thr_high, thr_low, pitch_high, pitch_low, roll_high, roll_low])
             try:
                 #self.ser_arduino.write(values)
-                self.ser_arduino.write('P %d %d %d %d %d %d' % (thr_high, thr_low, pitch_high, pitch_low, roll_high, roll_low))
+                #self.ser_arduino.write('P %d %d %d %d %d %d' % (thr_high, thr_low, pitch_high, pitch_low, roll_high, roll_low))
+                self.ser_arduino.write(struct.pack('>BBBBBB',thr_high,thr_low,pitch_high,pitch_low,roll_high,roll_low))
             except serial.SerialException as e:
                 rospy.logerr('Could not write to arduino')
                 return False
@@ -214,78 +230,11 @@ class uav_restriction():
             rospy.loginfo('Heading: %s', msg.yaw)
             rospy.loginfo('Heading: %s', msg.yaw*RAD2DEG)
 
-def restrict_uav():
-    # Variables
-    uav_pose = {
-        'x': 0,
-        'y': 0,
-        'z': 0,
-        'yaw':90
-    }
-    # Try changing rx_input below
-    rx_input = {
-        'throttle': 50,
-        'yaw': 50,
-        'pitch': 50,
-        'roll': 50,
-    }
-    # rx_output just defined as default values
-    rx_output = {
-        'throttle': 50,
-        'yaw': 50,
-        'pitch': 50,
-        'roll': 50,
-    }
-
-    print 'UAV pose:', uav_pose
-    print 'RX input:', rx_input
-
-    move_x_from_pitch = ( rx_input['pitch']-RX_CENTER_POSITION['pitch'] ) * sin( uav_pose['yaw']*pi / 180 )
-    move_y_from_pitch = ( rx_input['pitch']-RX_CENTER_POSITION['pitch'] ) * cos( uav_pose['yaw']*pi / 180 )
-    # print 'X component from pitch: %.02f' % move_x_from_pitch
-    # print 'Y component from pitch: %.02f' % move_y_from_pitch
-    if (DRONEBUR_LIMITS['x_min'] < uav_pose['x']+FACTOR_INPUT2METRIC_XY*move_x_from_pitch and uav_pose['x']+FACTOR_INPUT2METRIC_XY*move_x_from_pitch < DRONEBUR_LIMITS['x_max']) and (DRONEBUR_LIMITS['y_min'] < uav_pose['y']+FACTOR_INPUT2METRIC_XY*move_y_from_pitch and uav_pose['y']+FACTOR_INPUT2METRIC_XY*move_y_from_pitch < DRONEBUR_LIMITS['y_max']):
-        rx_output['pitch'] = rx_input['pitch']
-    else:
-        rx_output['pitch'] = RX_CENTER_POSITION['pitch']
-
-    move_x_from_roll = ( rx_input['roll']-RX_CENTER_POSITION['roll'] ) * sin( uav_pose['yaw']*pi / 180 + pi/2 )
-    move_y_from_roll = ( rx_input['roll']-RX_CENTER_POSITION['roll'] ) * cos( uav_pose['yaw']*pi / 180 + pi/2 )
-    # print 'X component from roll: %.02f' % move_x_from_roll
-    # print 'Y component from roll: %.02f' % move_y_from_roll
-    if (DRONEBUR_LIMITS['x_min'] < uav_pose['x']+FACTOR_INPUT2METRIC_XY*move_x_from_roll and uav_pose['x']+FACTOR_INPUT2METRIC_XY*move_x_from_roll < DRONEBUR_LIMITS['x_max']) and (DRONEBUR_LIMITS['y_min'] < uav_pose['y']+FACTOR_INPUT2METRIC_XY*move_y_from_roll and uav_pose['y']+FACTOR_INPUT2METRIC_XY*move_y_from_roll < DRONEBUR_LIMITS['y_max']):
-        rx_output['roll'] = rx_input['roll']
-    else:
-        rx_output['roll'] = RX_CENTER_POSITION['roll']
-
-    move_z_from_throttle = rx_input['throttle']-RX_CENTER_POSITION['throttle']
-    if DRONEBUR_LIMITS['z_min'] < uav_pose['z']+FACTOR_INPUT2METRIC_Z*move_z_from_throttle and uav_pose['z']+FACTOR_INPUT2METRIC_Z*move_z_from_throttle < DRONEBUR_LIMITS['z_max']:
-        rx_output['throttle'] = rx_input['throttle']
-    else:
-        rx_output['throttle'] = RX_CENTER_POSITION['throttle']
-
-    rx_output['yaw'] = rx_input['yaw']
-
-    UAV_new_x = uav_pose['x']+FACTOR_INPUT2METRIC_XY*(move_x_from_pitch + move_x_from_roll)
-    UAV_new_y = uav_pose['y']+FACTOR_INPUT2METRIC_XY*(move_y_from_pitch + move_y_from_roll)
-    UAV_new_z = uav_pose['z']+FACTOR_INPUT2METRIC_Z *move_z_from_throttle
-    print 'X component combined: %.02f' % UAV_new_y
-    print 'Y component combined: %.02f' % UAV_new_x
-    print 'Z component combined: %.02f' % UAV_new_z
-
-    if DRONEBUR_LIMITS['x_min'] < UAV_new_x and UAV_new_x < DRONEBUR_LIMITS['x_max']: print 'X: possible move'
-    else: print 'X: not possible'
-
-    if DRONEBUR_LIMITS['y_min'] < UAV_new_x and UAV_new_y <  DRONEBUR_LIMITS['y_max']: print 'Y: possible move'
-    else: print 'Y: not possible'
-
-    if DRONEBUR_LIMITS['z_min'] < UAV_new_z and UAV_new_z <  DRONEBUR_LIMITS['z_max']: print 'Z: possible move'
-    else: print 'Z: not possible'
-
-    print 'RX output:', rx_output
-
 if __name__ == '__main__':
-    try:
-        uav_restriction_module = uav_restriction()
-    except rospy.ROSInterruptException:
-        pass
+    if len(sys.argv) < 3:
+        print("usage: uav_restriction_node.py port baud")
+    else:
+        try:
+            uav_restriction_module = uav_restriction(sys.argv[1], sys.argv[2])
+        except rospy.ROSInterruptException:
+            pass
